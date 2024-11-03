453aa0f69b8a0642c7b8f8278b63bee01eef995c
Lucash123321 <ar.usa4yov@yandex.ru>
2024-11-04 00:50:07 +0300

    sign up was created

--- mtuci4/topics/urls.py
+++ mtuci4/topics/urls.py
@@ -4,9 +4,9 @@ from topics import views
 app_name = "topics"
 
 urlpatterns = [
-    path('', views.main, name='index'),
+    path('', views.main, name='main'),
+    path('topic/<str:slug>', views.topic, name='topic'),
+
     path('testtopic/', views.topic, name='test'),
     path('testpost/', views.post, name='post'),
-
-    path('<str:slug>', views.topic, name='topic'),
 ]
--- mtuci4/users/views.py
+++ mtuci4/users/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from .forms import UserRegistrationForm
+from django.views.generic import CreateView
+
 
 # Create your views here.
+class SignUpView(CreateView):
+    form_class = UserRegistrationForm
+    success_url = reverse_lazy('users:login')
+    template_name = 'users/signup.html'
