6df532d3f32440cb7e28f19e3412ab721b599b77
Lucash123321 <ar.usa4yov@yandex.ru>
2024-11-04 01:14:39 +0300

    CustomUser model

--- mtuci4/users/admin.py
+++ mtuci4/users/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from users import models
 
 # Register your models here.
+@admin.register(models.CustomUser)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', )
