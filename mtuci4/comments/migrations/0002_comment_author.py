a3cce4f3ceae3a3ab6ece9e452043f4a633111f7
Lucash123321 <ar.usa4yov@yandex.ru>
2024-11-04 14:55:45 +0300

    fix default value in models

--- mtuci4/comments/migrations/0002_comment_author.py
+++ mtuci4/comments/migrations/0002_comment_author.py
@@ -16,7 +16,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='comment',
             name='author',
-            field=models.ForeignKey(default='artyom', on_delete=django.db.models.deletion.CASCADE, related_name='comment_author', to=settings.AUTH_USER_MODEL),
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='comment_author', to=settings.AUTH_USER_MODEL),
             preserve_default=False,
         ),
     ]
--- mtuci4/posts/migrations/0002_post_author.py
+++ mtuci4/posts/migrations/0002_post_author.py
@@ -16,7 +16,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='post',
             name='author',
-            field=models.ForeignKey(default='artyom', on_delete=django.db.models.deletion.CASCADE, related_name='post_author', to=settings.AUTH_USER_MODEL),
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='post_author', to=settings.AUTH_USER_MODEL),
             preserve_default=False,
         ),
     ]
