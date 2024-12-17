d7d9a3553890ddc9678f24094a772ed3852e1e2e
maksanik <48986928+maksanik@users.noreply.github.com>
2024-12-17 20:41:34 +0300

    requested changes applied 2

--- mtuci4/topics/views.py
+++ mtuci4/topics/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render, get_object_or_404
-from django.db.models import Count, Q, Prefetch
+from django.db.models import Count, Q
 from topics.models import Topic
 
 
 # Create your views here.
 
 def topic(request, slug):
-    topic = Topic.objects.get(slug=slug)
-
+    topic = get_object_or_404(Topic, slug=slug)
+    
     posts = topic.posts.filter(topic=topic).annotate(
         upvotes=Count('votes', filter=Q(votes__vote_type='up')),
         downvotes=Count('votes', filter=Q(votes__vote_type='down'))
