d0a97795966878bd0b1c14d165587383eb1c66f7
maksanik <48986928+maksanik@users.noreply.github.com>
2024-12-19 17:35:12 +0300

    templates and styles simplification

--- mtuci4/mtuci4/settings.py
+++ mtuci4/mtuci4/settings.py
@@ -125,6 +125,7 @@ USE_24_HOUR_CLOCK = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
