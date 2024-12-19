d7db37b73fd12dcc0fc02b13b087857e886f84b9
maksanik <48986928+maksanik@users.noreply.github.com>
2024-12-19 20:37:36 +0300

    show/hide replies

---
+++ mtuci4/static/js/show_replies.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const showButtons = document.querySelectorAll(".comment-show-replies");
+
+    
+    const showReplies = (showButton) => {
+        const container = showButton.parentElement.parentElement.parentElement;
+        const replies = container.querySelector(".replies");
+
+        console.log(replies);
+        replies.classList.toggle("hidden");
+    }
+
+    showButtons.forEach(function (showButton) {
+        showButton.addEventListener("click", () => showReplies(showButton));
+    });
+});
